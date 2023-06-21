01443184edf7edc89e4d17a2fece970bbe4816b9
Ana Clara <anaclaranoronha.m@gmail.com>
2023-06-21 11:42:51 -0300

    configurando o roteamento

---
+++ dashboard/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Feedbacks = () => import('../views/Feedbacks/index.vue')
+const Home = () => import('../views/Home/index.vue')
+const Credentials = () => import('../views/Credentials/index.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/feedbacks',
+    name: 'Feedbacks',
+    component: Feedbacks,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/credentials',
+    name: 'Credentials',
+    component: Credentials
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: Home }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="text-brand-main">Home</h1>
+</template>
+<script>
+export default {}
+</script>
